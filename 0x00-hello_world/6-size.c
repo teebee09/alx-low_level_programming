722439e0c079e0787a3289fde416c34ed1b3d7ea
teebee09 <tijaniteebee09@gmail.com>
2022-09-08 20:45:53 -0700

    Programming low level language

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,11 @@
 
 int main(void)
 {
-	int i;
-	char j;
-	long int k;
-	long long int l;
-	float m;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(j));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(k));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(l));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(m));
-
-	return (0);
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+        return (0);
 }
