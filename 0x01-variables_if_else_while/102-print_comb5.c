4a4d1736a1d7b57af8d67b78b8e169357ee5c41c
teebee09 <tijaniteebee09@gmail.com>
2022-09-10 00:48:45 -0700

    Variable if and while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,15 +10,15 @@ int main(void)
 	int units;
 	int tens;
 
-	for (units = 0, units <= 98; units++)
+	for (units = 0; units <= 98; units++)
 	{
 		for (tens = units + 1; tens <= 99; tens++)
 		{
-			putchar((units / 10) * '0');
-			putchar((units % 10) * '0');
+			putchar((units / 10) + '0');
+			putchar((units % 10) + '0');
 			putchar(' ');
-			putchar((tens / 10) * '0');
-			putchar((tens % 10) * '0');
+			putchar((tens / 10) + '0');
+			putchar((tens % 10) + '0');
 
 			if (units == 98 && tens == 99)
 				continue;
