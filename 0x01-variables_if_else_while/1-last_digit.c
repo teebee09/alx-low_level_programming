9884fb1829f7701fe3c9c9e54d17d8a86d7f62c4
teebee09 <tijaniteebee09@gmail.com>
2022-09-09 20:46:26 -0700

    Variables if and while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,15 +24,15 @@ int main(void)
 
 		if (a > 5)
 		{
-			printf("and is greater than 5\n");
+			printf("greater than 5\n");
 		}
 		else if (a == 0)
 		{
-			printf("and is 0\n");
+			printf("0\n");
 		}
 		else
 		{
-			printf("and is less than 6 and not 0\n");
+			printf("less than 6 and not 0\n");
 		}
 
 		return (0);
