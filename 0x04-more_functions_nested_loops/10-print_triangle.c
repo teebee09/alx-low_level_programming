2f29037a96be4063efe19d02cc246f7367c285bb
teebee09 <tijaniteebee09@gmail.com>
2022-09-15 22:10:19 -0700

    Function nested loop

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,22 +8,25 @@
 
 void print_triangle(int size)
 {
-	int height;
-	int width;
+	int index;
 	int times;
 
 	if (size <= 0)
-		_putchar('\n');
-
-	for (height = 1; height <= size; height++)
 	{
-		for (width = 1; width <= (size - height); width++)
-			_putchar(' ');
+		for (times = 1; times <= size; times++)
+		{
+			for (index = size - times; index > 0; index++)
+				_putchar(' ');
 
-		for (times = 1; times <= height; times++)
-			_putchar('#');
+			for (index = 0; index < times; index++)
+				_putchar('#');
 
+			if (times == size)
+				continue;
 
-		_putchar('\n');
+			_putchar('\n');
+		}
 	}
+
+	_putchar('\n');
 }
