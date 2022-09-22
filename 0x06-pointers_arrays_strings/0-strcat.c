31dbb8e71eca1017378df81a801d5b19f95e1f9c
teebee09 <tijaniteebee09@gmail.com>
2022-09-21 21:57:32 -0700

    pointers array and string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -24,27 +24,3 @@ char *_strcat(char *dest, char *src)
 
 	return (dest);
 }
-
-/**
- * char dest[];
- * char src[];
- * int i, j;
- *
- * This loop is to store the length of dest in i
- * It just counts the number of characters in dest
- * You can also use strlen instead of this.
- *
- * for (i = 0; dest[i] != '\0'; ++i);
- *
- * This loop would concatenate the string dest at
- * the end of dest
- *
- * for (j = 0; src[i] != '\0'; ++j; ++i)
- * {
- * dest[i] = src[j];
- * }
- * \0 represents end of string
- * dest = '\0';
- * return (dest);
-*/
-
