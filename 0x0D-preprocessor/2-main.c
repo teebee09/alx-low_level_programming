29beedcf8ec540612eb940ce0266f9d808019d4b
teebee09 <tijaniteebee09@gmail.com>
2022-10-11 20:49:16 -0700

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * main - fncs that printsthe name of the file 
+ * main - fncs that printsthe name of the file
  *        it was compiled from
  * Return: return 0
  */
 
 int main(void)
 {
-	print("%s\n", __FILE__);
+	printf("%s\n", __FILE__);
 
 	return (0);
 }
