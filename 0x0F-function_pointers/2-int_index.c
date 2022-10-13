fb200ebd945900db3d5c23128c841c8150b4ff4d
teebee09 <tijaniteebee09@gmail.com>
2022-10-12 20:15:33 -0700

    function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,12 +13,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL && cmp == NULL)
+	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
 
 	/* iterate thru array and call searching function */
 	for (i = 0; i < size; i++)
-		if (cmp(array[i]) != 0)/* if returned true */
+		if (cmp(array[i]))/* if returned true */
 			return (i);
 
 	return (-1);
