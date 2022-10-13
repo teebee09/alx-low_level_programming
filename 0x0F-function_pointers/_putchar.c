8326aae0de69a3813cc2ecc0778baed09fde6516
teebee09 <tijaniteebee09@gmail.com>
2022-10-12 20:07:11 -0700

    function pointers

--- 0x0F-function_pointers/_putchar.c
+++ 0x0F-function_pointers/_putchar.c
@@ -4,6 +4,7 @@
  * _putchar - fncs that prints the character of a string
  * @c: the character to check
  * Return: return 0
+ * On error, -1 is returned, and errno is set appropriately
  */
 
 int _putchar(char c)
