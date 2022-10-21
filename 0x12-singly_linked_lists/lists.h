a64fa90aa13f79312ace645ab923858f1232df12
teebee09 <tijanitee09@gmail.com>
2022-10-21 14:33:11 -0700

    singly linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,6 +4,7 @@
 #include <stdio.h> /* size_t, printf */
 #include <stdlib.h> /* malloc */
 #include <string.h> /* strdup */
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
