193e4587253dde9f7698197c1cbc706b57722699
teebee09 <tijanitee09@gmail.com>
2022-10-21 15:01:27 -0700

    singly linked lists

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,10 +8,11 @@
 
 void free_list(list_t *head)
 {
-	if (head == NULL)
-		return;
-	if (head->next != NULL)
-		free_list(head->next);
-	free(head->str);
-	free(head);
+	while (head)
+	{
+		next = head->next;
+		free(head->str);
+		free(head);
+		head = next;
+	}
 }
