b2bb392b9597b12e2bafd5c70128dad8c7dd2184
teebee09 <tijanitee09@gmail.com>
2022-10-21 14:28:18 -0700

    singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,15 +13,11 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (null)\n");
-			h = h->next;
-		}
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-		}
+
+		h = h->next;
 		index++;
 	}
 	return (index);
