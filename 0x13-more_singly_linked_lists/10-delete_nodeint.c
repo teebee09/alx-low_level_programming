c158145fd58f62e841fd87c8b6cc4217e717665a
teebee09 <tijanitee09@gmail.com>
2022-10-24 21:07:33 -0700

    more singly linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,6 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(temp);
 		return (1);
 	}
+
 	/* iterate tmp to idx prior to idx we want to delete */
 	while (i < (index - 1) && temp != NULL)
 	{
