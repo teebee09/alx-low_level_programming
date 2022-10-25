64f5be964a716c34014a5a084cd3fab25232887a
teebee09 <tijanitee09@gmail.com>
2022-10-24 21:13:04 -0700

    more singly linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp, *temp2;
 
 	/* account for empty list */
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
 	temp = *head;
