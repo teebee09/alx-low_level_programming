fb6d91f55054fe336b5ee7ecc0d50d3ee536e4ee
teebee09 <tijanitee09@gmail.com>
2022-10-31 23:21:24 -0700

    file io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -44,10 +44,64 @@ int main(int argc, char *argv[])
 			close_error(fd_file_to);
 			exit(98);
 		}
+		/* check buffer end of file */
+		if (bytes_counted == 0)
+			break;
+		/* write the output in file_to and check write error */
+		bytes_counted = write(fd_file_to, buffer, bytes_counted);
+		if (bytes_counted < 0)
+		{
+			write_error(argv[2]);
+			close_error(fd_file_from);
+			close_error(fd_file_to);
+			exit(99);
+		}
 	}
+
+	/* close and check fd for file_from and file_to */
+	if (close_error(fd_file_from) < 0)
+	{
+		close_error(fd_file_to);
+		exit(100);
+	}
+	if (close_error(fd_file_to) < 0)
+		exit(100);
+	return (0);
 }
 
+/**
+ * close_error - close a file descriptor and check for a possible error.
+ * @fd: file descriptor for file to be closed.
+ * Return: 1 if fd colud be closeed, -1 if fd could not be closed.
+ */
 
+int close_error(int fd)
+{
+	int close_output;
+
+	close_output = close(fd);
+	if (close_output < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	return (close_output);
+}
+
+/**
+ * read_error - print the read error.
+ * @filename: filename.
+ */
+void read_error(char *filename)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+}
+
+/**
+ * write_error - print the write error.
+ * @filename: filename.
+ */
+void write_error(char *filename)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+}
 
 
 
